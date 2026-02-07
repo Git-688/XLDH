a859fa04672c75902fa07ce6fcdaa85b951a0c99
Git-688 <144002180+Git-688@users.noreply.github.com>
2026-02-07 15:49:55 +0800

    Add files via upload

---
+++ pages/chl/ysxz.js
@@ -0,0 +1,83 @@
+// 模块化数据 - 影视资源列表
+const movieData = [
+    {
+        id: 1,
+        title: "流浪地球2",
+        type: "科幻/灾难",
+        duration: "173分钟",
+        year: "2023",
+        resolution: "1080P",
+        img: "https://picsum.photos/id/1/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 2,
+        title: "满江红",
+        type: "悬疑/历史",
+        duration: "159分钟",
+        year: "2023",
+        resolution: "4K",
+        img: "https://picsum.photos/id/2/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 3,
+        title: "复仇者联盟4",
+        type: "动作/科幻",
+        duration: "181分钟",
+        year: "2019",
+        resolution: "1080P",
+        img: "https://picsum.photos/id/3/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 4,
+        title: "蜘蛛侠：纵横宇宙",
+        type: "动画/冒险",
+        duration: "140分钟",
+        year: "2023",
+        resolution: "2K",
+        img: "https://picsum.photos/id/4/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 5,
+        title: "长安三万里",
+        type: "动画/历史",
+        duration: "168分钟",
+        year: "2023",
+        resolution: "1080P",
+        img: "https://picsum.photos/id/5/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 6,
+        title: "速度与激情10",
+        type: "动作/犯罪",
+        duration: "142分钟",
+        year: "2023",
+        resolution: "4K",
+        img: "https://picsum.photos/id/6/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 7,
+        title: "芭比",
+        type: "喜剧/奇幻",
+        duration: "114分钟",
+        year: "2023",
+        resolution: "1080P",
+        img: "https://picsum.photos/id/7/300/160",
+        downloadUrl: "#"
+    },
+    {
+        id: 8,
+        title: "奥本海默",
+        type: "传记/历史",
+        duration: "180分钟",
+        year: "2023",
+        resolution: "2K",
+        img: "https://picsum.photos/id/8/300/160",
+        downloadUrl: "#"
+    }
+];
